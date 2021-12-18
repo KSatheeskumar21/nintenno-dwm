917c9eea3c70a9a3a4f4cbf8862436e821133bd9
Kishore Satheeskumar <k.sath214@gmail.com>
2021-12-18 22:16:04 +0000

    Initial commit

---
+++ themes/doom-one.h
@@ -0,0 +1,7 @@
+static const char col_gray1[]       = "#222222";
+static const char col_gray2[]       = "#444444";
+static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray4[]       = "#eeeeee";
+static const char col_gray5[] 	    = "#6790EB";
+static const char col_cyan[]        = "#c678dd";
+
---
+++ themes/tokyo-night.h
@@ -0,0 +1,6 @@
+static const char col_gray1[]       = "#1a1b26";
+static const char col_gray2[]       = "#1a1b26";
+static const char col_gray3[]       = "#f8f8f2";
+static const char col_gray4[]       = "#f8f8f2";
+static const char col_gray5[] 	    = "#2ac3de";
+static const char col_cyan[]        = "#7aa2f7";
